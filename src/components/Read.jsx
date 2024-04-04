f0b596eff9cd11d8cf567da732f341c90d28def1
Vivek Raj Srivastava <74672080+256bitguy@users.noreply.github.com>
2024-04-04 20:05:14 +0530

    Update Read.jsx

--- src/components/Read.jsx
+++ src/components/Read.jsx
@@ -1,30 +1,40 @@
-import { useEffect } from "react"
-import { useDispatch, useSelector } from "react-redux"
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { showUser } from "../features/userDetailSlice";
 
 const Read = () => {
-    const dispatch=useDispatch();
-    useEffect(()=>{
-        dispatch(showUser())
-    },[]);
-    const allData=useSelector((state)=>state.app.users);
-  return (
-    <div>{
-       allData.map((item)=>{
-        return(
-            <>
-            <div className="card w-50 mx-auto"  >
-            <div className="card-body">
-                <h5 className="card-title">{item.name}</h5>
-                <h6 className="card-subtitle mb-2 text-body-secondary">{item.email}</h6>
-                <p className="card-text">{item.email}</p>
-                 
-            </div>
-        </div></>
-        )
-       })}
-    </div>
-  )
-}
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(showUser());
+    }, []);
 
-export default Read
+    const { users, loading } = useSelector((state) => state.app);
+
+    if (loading) {
+        return (
+            <div className="w-50 mx-auto"> <h2>LOADING!!</h2></div>
+        );
+    }
+
+    // Reverse the users array
+    const reversedUsers = users.slice().reverse();
+
+    return (
+        <div>
+            {reversedUsers &&
+                reversedUsers.map((item, index) => {
+                    return (
+                        <div key={index} className="card w-50 mx-auto">
+                            <div className="card-body">
+                                <h5 className="card-title">{item.name}</h5>
+                                <h6 className="card-subtitle mb-2 text-body-secondary">{item.email}</h6>
+                                <p className="card-text">{item.email}</p>
+                            </div>
+                        </div>
+                    );
+                })}
+        </div>
+    );
+};
+
+export default Read;
